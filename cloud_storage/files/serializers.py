86130c78157b6c4c45a6e2c49cb5921554c51c19
k3ssL <viqwer3@yandex.ru>
2024-02-28 23:03:54 +0500

    rudiment

--- cloud_storage/files/serializers.py
+++ cloud_storage/files/serializers.py
@@ -7,21 +7,20 @@ class FileSerializer(serializers.ModelSerializer):
     class Meta:
         model = UploadedFile
         fields = ['user', 'file', 'file_id', 'name']
+        extra_kwargs = {
+            'name': {'required': False}
+        }
 
         def create(self, validated_data):
             user = self.context['request'].user
-            print(user)
             uploaded_file = validated_data['file']
             instance = UploadedFile(user=user, file=uploaded_file)
             instance.save()
             return instance
-
-        def update(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.fields['name'].required = True  # Делаем поле 'name' обязательным при обновлении
 
 
 class FileAccessSerializer(serializers.ModelSerializer):
     class Meta:
         model = FileAccess
         fields = ['user', 'access_type']
+
