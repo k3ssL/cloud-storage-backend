4ae33772227a36935dd9f5ead421bbcc96380f4e
k3ssL <viqwer3@yandex.ru>
2024-02-28 23:03:16 +0500

    add new url

--- cloud_storage/files/urls.py
+++ cloud_storage/files/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import FileUploadAPIView, FileEditAPIView, FileAccessAPIView
+from .views import FileUploadAPIView, FileEditAPIView, FileAccessAPIView, UserFilesView, SharedFilesView
 
 app_name = 'files'
 
@@ -7,4 +7,6 @@ urlpatterns = [
     path('files', FileUploadAPIView.as_view(), name='file-upload'),
     path('files/<int:id>/', FileEditAPIView.as_view(), name='file-edit'),
     path('files/<int:id>/accesses/', FileAccessAPIView.as_view(), name='file-access'),
+    path('files/disk/', UserFilesView.as_view(), name='user-files'),
+    path('shared', SharedFilesView.as_view(), name='shared-files'),
 ]
